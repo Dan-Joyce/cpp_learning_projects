cbd6c7430f40a4a2219e4a29f51b0c76e3da421c
Dan-Joyce <favourbertina@gmail.com>
2025-10-15 21:45:10 +0200

    Jour 1 : Hello World

---
+++ jour1_hello_world/jour1_hello_world/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Bienvenue dans ton premier programme C++ YEAH !!!" << endl;
+    cout << "Hello World !" << endl;
+    return 0;
+}
